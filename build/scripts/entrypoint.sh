ce06842f7d93d7c6a3082b79e8e35fe50e3fa25c
JtMotoX <7191259+JtMotoX@users.noreply.github.com>
2022-04-25 22:24:59 -0700

    adding 1password integration support

--- build/scripts/entrypoint.sh
+++ build/scripts/entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+# CONVERT 1PASSWORD ENVIRONMENT VARIABLES
+if env | grep -E '^[^=]*=OP:' >/dev/null; then
+	curl -s -o /tmp/1password-vars.sh "https://raw.githubusercontent.com/JtMotoX/1password-docker/main/1password/op-vars.sh"
+	chmod 755 /tmp/1password-vars.sh
+	. /tmp/1password-vars.sh
+	rm -f /tmp/1password-vars.sh
+fi
+
 touch /var/log/honeywell-logger/honeywell-logger.log
 tail -f -n0 /var/log/honeywell-logger/honeywell-logger.log &
 
