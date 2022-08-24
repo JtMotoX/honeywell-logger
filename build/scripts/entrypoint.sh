cfb705b9710fc72f00a170a32726a18bd0cd4af8
JtMotoX <7191259+JtMotoX@users.noreply.github.com>
2022-08-23 18:31:43 -0700

    run as non-root user

--- build/scripts/entrypoint.sh
+++ build/scripts/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 # CONVERT 1PASSWORD ENVIRONMENT VARIABLES
 if env | grep -E '^[^=]*=OP:' >/dev/null; then
 	curl -sS -o /tmp/1password-vars.sh "https://raw.githubusercontent.com/JtMotoX/1password-docker/main/1password/op-vars.sh"
@@ -8,10 +10,29 @@ if env | grep -E '^[^=]*=OP:' >/dev/null; then
 	rm -f /tmp/1password-vars.sh
 fi
 
-touch /var/log/honeywell-logger/honeywell-logger.log
-tail -f -n0 /var/log/honeywell-logger/honeywell-logger.log &
+if [ "$1" = "run" ]; then
+	# MAKE SURE WE HAVE ACCESS TO LOGS
+	LOG_FILE="/var/log/honeywell-logger/honeywell-logger.log"
+	touch "${LOG_FILE}" 2>/dev/null || true
+	if [ ! -w ${LOG_FILE} ]; then
+		echo "ERROR: Getting access denied to log files."
+		echo "NOTE: 'chmod -R 777 logs'"
+		exit 1
+	fi
+	tail -f -n0 "${LOG_FILE}" &
 
-touch /tmp/crond.log
-tail -f /tmp/crond.log &
+	# MAKE SURE WE HAVE ACCESS TO CONFIGS
+	if ! cat /configs/honeywell.d/*.conf >/dev/null 2>&1; then
+		echo "ERROR: Getting access denied to honeywell.d config files"
+		echo "Either you did not create a conf file within your honeywell.d directory, or you need to set the correct file permissions ('chmod -R 777 configs')"
+		exit 1
+	fi
 
-crond -f -l 8 -L /tmp/crond.log
+	touch /tmp/crond.log
+	tail -f /tmp/crond.log &
+
+	supercronic ~/crontab >/tmp/crond.log 2>&1
+	exit 1
+fi
+
+exec "$@"
